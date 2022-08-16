a8d1972946792805ab2d86c23da860e040b7e448
Jeffrey Vaughn <jvaughn1230@gmail.com>
2022-08-16 09:12:25 -0700

    added tailwind

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <nav>
-        <div>Logo</div>
-        <div>
+    <nav className='flex pt-5 justify-between content-center'>
+        <div className=''>Logo</div>
+        <div className='w-100 flex justify-around'>
             <a href='#about'>About</a>
             <a href='#skills'>Skills</a>
             <a href='#projects'>Projects</a>
