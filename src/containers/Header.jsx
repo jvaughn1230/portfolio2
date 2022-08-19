293c0fe760480cb37a5964f2f729d8db7fbb2eda
Jeffrey Vaughn <jvaughn1230@gmail.com>
2022-08-19 12:51:28 -0700

    added background image and styling

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from 'react';
+
+import Navbar from '../components/Navbar';
+import Hero from '../components/Hero';
 
 function Header() {
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center bg-cover bg-[url('{require('../img/hero-bg.png')} ')">
-        <div className='text-6xl font-medium'>Hello, I'm Jeffrey Vaughn!</div>
-        <hr className='h-1  bg-ocean my-6 mx-auto w-3/12' />
-        <div className='text-lg font-normal'>A fullstack developer based in Las Vegas, NV</div>
+    <div className='hero-bg'>
+      <Navbar />
+      <Hero />
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
