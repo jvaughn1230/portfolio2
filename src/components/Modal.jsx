e1b498896483dcb984773fce20f1e7caadae6dcb
Jeffrey Vaughn <jvaughn1230@gmail.com>
2022-08-25 06:44:43 -0700

    added basic popup modal outline

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
 
-function Modal() {
+// import "./Modal.css";
+
+
+
+function Modal({closeModal}, props) {
   return (
-    <div>Modal</div>
-  )
+    <div onClick={()=>{closeModal(false)}}className='fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center'>
+      <div className='bg-white p-2 rounded'>
+
+        {console.log("modal", props)}
+        <div className='titleCloseBtn'>
+          <button onClick={()=>{closeModal(false)}}>X</button>
+        </div>
+
+        <div className='title'>
+          Title: {props.title}
+        </div>
+
+        <div className='body'>
+          {props.subTitle}
+        </div>
+
+        <div className='footer'>
+          Footer Example
+        </div>
+
+      </div>
+    </div>
+  );
 }
 
-export default Modal
+export default Modal;
