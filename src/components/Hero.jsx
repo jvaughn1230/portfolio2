a5a7cd9a4d43899e6b841d93903f10dfdd4ca842
Jeffrey Vaughn <jvaughn1230@gmail.com>
2022-08-25 07:35:30 -0700

    add mobile version of navbar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import Slash from './Slash';
 
 function Hero() {
   return (
-    <div className='min-h-screen flex flex-col justify-center items-center px-10 text-light'>
+    <div className='min-h-screen flex flex-col justify-center items-center px-5 md:px-10 text-light'>
         <div className='text-6xl font-medium'>Hello, I'm Jeffrey Vaughn!</div>
         <Slash />
         <div className='text-lg font-normal'>A fullstack developer based in Las Vegas, NV</div>
--- src/components/Slash.jsx
+++ src/components/Slash.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Slash() {
   return (
-    <hr className='h-1  bg-ocean my-6 mx-auto w-14 border-black border-none rounded-md' />
+    <hr className='h-1  bg-ocean my-6 mx-auto w-14 border-black border-none rounded-md text-center' />
   )
 }
 
